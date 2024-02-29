abb57676955c355222ce3adf99c4e9bc023cfb43
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-02-29 14:54:51 +0100

    implemented equal() and lcm(), started work on add()

--- fract.cpp
+++ fract.cpp
@@ -1,6 +1,5 @@
 #include "fract.h"
 
-
 fract::fract(int num, unsigned int denom)
     : numerator(num), denominator(denom)
 {
@@ -22,15 +21,69 @@ unsigned int fract::denom() const
     return denominator;
 }
 
-void fract::print(std::ostream& os) const
+void fract::print(std::ostream &os) const
 {
     os << num();
     if (denom() != 1)
         os << '/' << denom();
 }
 
+bool fract::equal(const fract &right) const
+{
+    if (numerator == right.num() && denominator == right.denom())
+        return true;
+    return false;
+}
+
+fract fract::neg() const
+{
+    return fract(-numerator, denominator);
+}
+
+void fract::add(const fract &right)
+{
+    unsigned int commonDenom = lcm(denominator, right.denom());
+
+    int leftNumerator = commonDenom / denominator;
+    int rightNumerator = commonDenom / right.denom();
+
+    numerator = leftNumerator + rightNumerator;
+    denominator = commonDenom;
+}
+
+void fract::reduceFraction()
+{
+    // skracanie ułamka ?
+    unsigned int divisor = gcd(numerator, denominator);
+    if (divisor > 1)
+    {
+        numerator /= divisor;
+        denominator /= divisor;
+    }
+}
+
+/* TODO:
+-equals
+-neg
+-add
+-sum
+-mul <- multiply
+-prod <- zwraca ułamek, bierze ułamek
+
+dodatkowo:
+- skroc ulamek
+
+*/
+
+unsigned int lcm(unsigned int a, unsigned int b)
+{
+    // least common multiple
+    return (a * b) / gcd(a, b);
+}
+
 unsigned int gcd(unsigned int a, unsigned int b)
 {
+    // greatest common divisor
     while (a > 0 && b > 0)
     {
         if (a > b)
