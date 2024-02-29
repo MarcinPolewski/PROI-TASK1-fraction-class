c57fc387dea11bf936e30c602330c459049a4d8f
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-02-29 14:53:32 +0100

    added tests for equal(), neg(), lcm() and add()

--- fract_test.cpp
+++ fract_test.cpp
@@ -28,6 +28,172 @@ int main()
         std::cout << std::endl;
     }
 
+    // testing equal()
+    std::cerr << "equal() tests:\n";
+    {
+        fract f1(2, 4);
+        fract f2(4, 8);
+
+        if (!f1.equal(f2))
+        {
+            std::cerr << "Error, values are equal\n";
+        }
+    }
+
+    {
+        fract f1(1, 3);
+        fract f2(1, 3);
+
+        if (!f1.equal(f2))
+        {
+            std::cerr << "Error, values are equal\n";
+        }
+    }
+
+    {
+        fract f1(0, 3);
+        fract f2(0, 1000);
+
+        if (!f1.equal(f2))
+        {
+            std::cerr << "Error, values are equal\n";
+        }
+    }
+
+    {
+        fract f1(1, 3);
+        fract f2(1, 2);
+
+        if (f1.equal(f2))
+        {
+            std::cerr << "Error, values are not equal\n";
+        }
+    }
+
+    {
+        fract f1(1, 3);
+        fract f2(0, 2);
+
+        if (f1.equal(f2))
+        {
+            std::cerr << "Error, values are not equal\n";
+        }
+    }
+
+    // testing neg()
+    std::cerr << "neg tests:\n";
+    {
+        fract f1(1, 2);
+        fract f2 = f1.neg();
+
+        if (f2.num() != -1 or f2.denom() != 2)
+        {
+            std::cerr << "Error, invalid fraction\n";
+        }
+        f2.print(std::cout);
+        std::cout << std::endl;
+    }
+
+    {
+        fract f1(-1, 2);
+        fract f2 = f1.neg();
+
+        if (f2.num() != 1 or f2.denom() != 2)
+        {
+            std::cerr << "Error, invalid fraction\n";
+        }
+        f2.print(std::cout);
+        std::cout << std::endl;
+    }
+
+    {
+        fract f1(0, 2);
+        fract f2 = f1.neg();
+
+        if (f2.num() != 0 or f2.denom() != 1)
+        {
+            std::cerr << "Error, invalid fraction\n";
+        }
+        f2.print(std::cout);
+        std::cout << std::endl;
+    }
+
+    // testing lcm
+    std::cerr << "testing lcm\n";
+    {
+        unsigned int a = 3;
+        unsigned int b = 4;
+
+        if (lcm(a, b) != 12)
+            std::cerr << "Error, with lcm of " << a << " " << b << '\n';
+    }
+
+    {
+        unsigned int a = 6;
+        unsigned int b = 4;
+
+        if (lcm(a, b) != 12)
+            std::cerr << "Error, with lcm of " << a << " " << b << '\n';
+    }
+
+    {
+        unsigned int a = 5;
+        unsigned int b = 10;
+
+        if (lcm(a, b) != 10)
+            std::cerr << "Error, with lcm of " << a << " " << b << '\n';
+    }
+
+    // testing add
+    std::cerr << "add tests:\n";
+    {
+        fract f1(1, 2);
+        fract f2(1, 3);
+
+        f1.add(f2);
+
+        if (f1.num() != 5 && f1.denom() != 6)
+            std::cerr << "Error, wrong addition result!\n";
+        f2.print(std::cout);
+        std::cout << '\n';
+    }
+
+    {
+        fract f1(-1, 2);
+        fract f2(1, 2);
+
+        f1.add(f2);
+
+        if (f1.num() != 0 && f1.denom() != 1)
+            std::cerr << "Error, wrong addition result!\n";
+        f2.print(std::cout);
+        std::cout << '\n';
+    }
+
+    {
+        fract f1(1, 2);
+        fract f2(-1, 4);
+
+        f1.add(f2);
+
+        if (f1.num() != 1 && f1.denom() != 4)
+            std::cerr << "Error, wrong addition result!\n";
+        f2.print(std::cout);
+        std::cout << '\n';
+    }
+
+    {
+        fract f1(-1, 2);
+        fract f2(-1, 4);
+
+        f1.add(f2);
+
+        if (f1.num() != 1 && f1.denom() != 4)
+            std::cerr << "Error, wrong addition result!\n";
+        f2.print(std::cout);
+        std::cout << '\n';
+    }
+
     std::cout << std::endl;
     return 0;
 }
